47b9efd61a376bc05520ac2afc62e19ac8078534
Elyar-es <111648569+Elyar-es@users.noreply.github.com>
2022-11-18 12:18:16 +0330

    Create a TCP socket

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,6 @@
+use std::io::Read;
+use std::net::TcpListener;
+
 pub struct Server {
     addr: String,
 }
@@ -11,7 +14,36 @@ impl Server {
     }
 
     pub fn run(self) {
-        println!("listening on {}", self.addr)
+        println!("listening on {}", self.addr);
+
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+
+            match listener.accept() {
+                
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                        },
+
+                        Err(e) => {
+                            println!("Failed to read from a connection: {}", e);
+                        },
+
+                    }
+                },
+                Err(e) => {
+                    println!("Failed to establish a connection: {}", e);
+                }
+
+            }
+
+        }
+
     }
 
 }
--- target/debug/deps/server-5db9682f58fc818b.d
+++ target/debug/deps/server-5db9682f58fc818b.d
@@ -1,6 +1,6 @@
-/Users/elyaresmailzadeh/Documents/toTrash/rust/server/target/debug/deps/server-5db9682f58fc818b.rmeta: src/main.rs src/http/mod.rs src/http/method.rs src/http/request.rs src/server.rs
+/Users/elyaresmailzadeh/Documents/GitHub/server-1/target/debug/deps/server-5db9682f58fc818b.rmeta: src/main.rs src/http/mod.rs src/http/method.rs src/http/request.rs src/server.rs
 
-/Users/elyaresmailzadeh/Documents/toTrash/rust/server/target/debug/deps/server-5db9682f58fc818b.d: src/main.rs src/http/mod.rs src/http/method.rs src/http/request.rs src/server.rs
+/Users/elyaresmailzadeh/Documents/GitHub/server-1/target/debug/deps/server-5db9682f58fc818b.d: src/main.rs src/http/mod.rs src/http/method.rs src/http/request.rs src/server.rs
 
 src/main.rs:
 src/http/mod.rs:
